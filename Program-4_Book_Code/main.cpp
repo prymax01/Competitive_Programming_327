6db7c835e4652a2bb8cc1bd0cf76f6c818820f90
Shubhranil Sarkar <shubhranilsarka1@gmail.com>
2026-09-17 15:28:41 +0530

    Library Book Code Validator

--- Program-4_Book_Code/main.cpp
+++ Program-4_Book_Code/main.cpp
@@ -1,1 +1,43 @@
+#include <iostream>
+#include <string>
+#include <cctype>
+using namespace std;
 
+int main() {
+    string code;
+    cin >> code;
+
+    bool valid = true;
+
+    if (code.length() != 12)
+        valid = false;
+    else {
+        for (int i = 0; i < 3; i++) {
+            if (!isupper(code[i]))
+                valid = false;
+        }
+
+        if (code[3] != '-')
+            valid = false;
+
+        for (int i = 4; i < 8; i++) {
+            if (!isdigit(code[i]))
+                valid = false;
+        }
+
+        if (code[8] != '-')
+            valid = false;
+
+        for (int i = 9; i < 12; i++) {
+            if (!isdigit(code[i]))
+                valid = false;
+        }
+    }
+
+    if (valid)
+        cout << "Valid book code";
+    else
+        cout << "Invalid book code";
+
+    return 0;
+}
