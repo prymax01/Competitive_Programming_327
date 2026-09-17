89e09d141ffab824fb584ebd9567d0541d51b9d2
Shubhranil Sarkar <shubhranilsarka1@gmail.com>
2026-09-17 15:27:59 +0530

    E-Commerce Product Price Sorting
    Implement price sorting using a simple bubble sort algorithm.

--- Program-5_Price_Sorting/main.cpp
+++ Program-5_Price_Sorting/main.cpp
@@ -1,1 +1,27 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+    int n;
+    cin >> n;
+
+    double price[n];
+
+    for (int i = 0; i < n; i++)
+        cin >> price[i];
+
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = i + 1; j < n; j++) {
+            if (price[i] > price[j]) {
+                double temp = price[i];
+                price[i] = price[j];
+                price[j] = temp;
+            }
+        }
+    }
+
+    for (int i = 0; i < n; i++)
+        cout << price[i] << " ";
+
+    return 0;
+}
